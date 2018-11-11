7de65907d600328e39322cb23f33a182d7b15610
SanzianaTudose <tudosesanziana@gmail.com>
2018-11-11 02:17:06 +0200

    + Fragments: navigation change

---
+++ app/src/main/java/com/plantae/bloodify/MainActivity.java
@@ -0,0 +1,66 @@
+package com.plantae.bloodify;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.plantae.bloodify.NavigationFragments.EventsFragment;
+import com.plantae.bloodify.NavigationFragments.HomeFragment;
+import com.plantae.bloodify.NavigationFragments.InfoFragment;
+import com.plantae.bloodify.NavigationFragments.LeaderboardFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Fragment fragment = null;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.navigation_leaderboard:
+                    fragment = new LeaderboardFragment();
+                    break;
+                case R.id.navigation_events:
+                    fragment = new EventsFragment();
+                    break;
+                case R.id.navigation_info:
+                    fragment = new InfoFragment();
+                    break;
+            }
+
+            return loadFragment(fragment);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadFragment(new HomeFragment());
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+}
