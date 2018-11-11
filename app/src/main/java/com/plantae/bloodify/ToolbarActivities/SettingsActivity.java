883b7f8de2a55dad162ad7e2d6ab11596077c102
SanzianaTudose <tudosesanziana@gmail.com>
2018-11-11 11:20:36 +0200

    + SettingsActivity layout

--- app/src/main/java/com/plantae/bloodify/ToolbarActivities/SettingsActivity.java
+++ app/src/main/java/com/plantae/bloodify/ToolbarActivities/SettingsActivity.java
@@ -6,12 +6,17 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.plantae.bloodify.MainActivity;
 import com.plantae.bloodify.QRCodeActivity;
 import com.plantae.bloodify.R;
 
+import org.honorato.multistatetogglebutton.MultiStateToggleButton;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    MultiStateToggleButton toggleGrupa = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
